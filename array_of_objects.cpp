15a77d5b821e56c758476c60d3de485c5b5e7548
asus test <msa-99@outlook.com>
2024-05-28 23:40:50 +0600

    object from class

---
+++ array_of_objects.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Student {
+    public:
+    string name;
+    int clss;
+    int marks;
+    //ekhane constructor function neya lagbe na
+};
+int main(){
+    cout<<"MSA";
+    return 0;
+}
