607b2f4326e740fe6d9d451cb8550f0796e34391
asus test <msa-99@outlook.com>
2024-05-28 23:53:03 +0600

    take input make array of object

--- array_of_objects.cpp
+++ array_of_objects.cpp
@@ -7,7 +7,22 @@ class Student {
     int marks;
     //ekhane constructor function neya lagbe na
 };
+
 int main(){
-    cout<<"MSA";
+    int n;
+    cin>>n;
+    //create dynamic array of objects
+    Student* a=new Student[n];
+    // ekhane a ekti n songkhok object er array. ar protiti object hocce Student type er zekhane name, clss and marks thakbe.
+
+    //run loop for n-1 times and get input for every objects
+    for(int i=0; i<n; i++){
+        getchar();
+        getline(cin, a[i].name);
+        cin>>a[i].clss>>a[i].marks;
+    }
+    for(int i=0; i<n; i++){
+        cout<<a[i].name<<" "<<a[i].clss<<" "<<a[i].marks<<endl;
+    }
     return 0;
 }
